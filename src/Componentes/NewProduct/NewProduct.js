a4742dcd55c2f4026983c07da05b2867e1609c85
Heidi Rivas <heidift8@gmail.com>
2022-08-23 12:26:13 -0400

    inputs

--- src/Componentes/NewProduct/NewProduct.js
+++ src/Componentes/NewProduct/NewProduct.js
@@ -3,13 +3,26 @@ import styles from './newProduct.module.css'
 
 
 const NewProduct = () => {
-  return (
+const campos= ['categorias', 'SKU_seller','título','descripcion','imagen1', 'imagen-Miniatura','variant_id','marca','color','talla','tabla-de-tallas','género'];
+return (
     <main>
-    <div><h1>NewProduct</h1>
+    <section className={styles.container}>
     
+    {campos.map((item) => (
+      <div className={styles.item}>
+          <p>{item} </p>  
+          <input
+         className={styles.inputcampos}
+         name={item}
+         type='text'
+       ></input>
+       </div>
+         ))}
+
+    
+    </section>
     <button>Cargar</button>
     
-    </div>
     </main>
   );
 };
