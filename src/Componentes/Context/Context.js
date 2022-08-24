e787a8f95cf0ed6fd4734f8680aadbabff198469
Heidi Rivas <heidift8@gmail.com>
2022-08-24 19:19:42 -0400

    inputs con values

---
+++ src/Componentes/Context/Context.js
@@ -0,0 +1,22 @@
+/*const enviarDatos = (event) =>{
+    event.preventDefault();
+    const inpVal= {
+       categoria:datos.categoria,
+       SKU_seller:datos.SKU_seller,
+       título:datos.título,
+       descripcion:datos.descripcion,
+       imagen1:datos.imagen1,
+       imagen_Miniatura:datos.imagen_Miniatura,
+       variant_id: datos.variant_id,
+       marca:datos.marca,
+       color:datos.color,
+       talla:datos.talla,
+       tabla_de_tallas: datos.tabla_de_talla,
+       genero:datos.genero}
+       }
+
+       crear un estado 
+
+       crear un array vacio en un estado data SsetData([])
+
+       lo llamo dentro una funcion esta funcion tiene dentro de si al setdata y ese setdata guarda lo q recoge enviar datos la funcion tiene como parametro un objeto , del lado de newproduct enviardatos llama a la funcion del context y te entrego la variable q cree con el objeto. */
--- src/Pages/NewProduct/NewProduct.js
+++ src/Pages/NewProduct/NewProduct.js
@@ -1,47 +1,78 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Header from '../../Componentes/Header/Header';
 import styles from './newProduct.module.css'
 
 
+
 const NewProduct = () => {
-  const campos = [{"nombre": "categoria","descripcion": "A que tipo de objeto pertence el artículo"},
-                  {"nombre": 'SKU_seller',"descripcion": "Número único que identifica su producto"} , 
-                  {"nombre":  'título',"descripcion": "Nombre del producto"} ,
-                  {"nombre":  'descripcion',"descripcion": "Explique detalladamente las características del producto"}, 
-                  {"nombre":   'imagen1',"descripcion": "Agregue imagen de buena calidad"},
-                  {"nombre":   'imagen-Miniatura',"descripcion": "Agregue imagen de buena calidad"},
-                  {"nombre":   'variant_id',"descripcion": "Número que identifica la variable de su producto, de existir"},
-                  {"nombre":   'marca',"descripcion": "Nombre de la marca de su producto"},
-                  {"nombre":   'color',"descripcion": "Indique el color"},
-                  {"nombre":   'talla',"descripcion": "Agregue talla disponible"},
-                  {"nombre":   'tabla-de-tallas',"descripcion": "Ingrese tabla de tallas"},
-                  {"nombre":   'genero',"descripcion": "Agregue imagen de buena calidad"},
-                 ]
+  const campos = [{ "nombre": "categoria", "descripcion": "A que tipo de objeto pertence el artículo" },
+  { "nombre": 'SKU_seller', "descripcion": "Número único que identifica su producto" },
+  { "nombre": 'título', "descripcion": "Nombre del producto" },
+  { "nombre": 'descripcion', "descripcion": "Explique detalladamente las características del producto" },
+  { "nombre": 'imagen1', "descripcion": "Agregue imagen de buena calidad" },
+  { "nombre": 'imagen_Miniatura', "descripcion": "Agregue imagen de buena calidad" },
+  { "nombre": 'variant_id', "descripcion": "Número que identifica la variable de su producto, de existir" },
+  { "nombre": 'marca', "descripcion": "Nombre de la marca de su producto" },
+  { "nombre": 'color', "descripcion": "Indique el color" },
+  { "nombre": 'talla', "descripcion": "Agregue talla disponible" },
+  { "nombre": 'tabla_de_tallas', "descripcion": "Ingrese tabla de tallas" },
+  { "nombre": 'genero', "descripcion": "Agregue imagen de buena calidad" },
+  ]
+  const [datos, setDatos] = useState({
+    categoria:'',
+    SKU_seller:'',
+    título:'',
+    descripcion:'',
+    imagen1:'',
+    imagen_Miniatura:'',
+    variant_id:'',
+    marca:'',
+    color:'',
+    talla:'',
+    tabla_de_tallas:'',
+    genero:''
+  })
+
+  const handleInputChange = (event) =>{
+      setDatos({
+        ...datos,
+        [event.target.name] : event.target.value
+
+      })
+}
   return (
     <main>
       <Header />
       <div>
         <h1>NewProduct</h1>
-        <section className={styles.container}>
+        <form >
+          <section className={styles.container}>
 
-          {campos.map((item) => (
-            <div className={item.nombre}>
-              <p>{item.nombre} </p>
-              <p>{item.descripcion}</p>
-              <input
-                className={styles.inputcampos}
-                name={item}
-                type='text'
-              ></input>
-            </div>
-          ))}
+            {campos.map((item) => (
+              <div className={item.nombre}>
+                <p>{item.nombre} </p>
+                <p>{item.descripcion}</p>
+                <input
+                  className={styles.inputcampos}
+                  name={item.nombre}
+                  type='text'
+                  onChange={handleInputChange}
+                ></input>
+              </div>
+            ))}
 
+            <button type="submit" >Cargar</button>
+          </section>
+        </form>
 
-        </section>
-        <button>Cargar</button>
       </div>
+
     </main>
+
   );
 };
 
 export default NewProduct;
+
+
+
