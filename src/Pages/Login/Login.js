d130a926027d300e6848ff2a933489247297d274
unknown <mariannybatista2003@gmail.com>
2022-08-23 22:42:17 -0400

    LOGIN

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,7 @@
 import styles from './login.module.css'
 import Logo from '../../Componentes/imagenes/logo-Ripley.png'
 import { Link } from 'react-router-dom'
+import Bienvenido from '../../Componentes/imagenes/Bienvenido.png'
 
 const Login = () => {
 	  
@@ -10,13 +11,14 @@ const Login = () => {
 				<section className={styles.sct}>
 				<form className={styles.login_form}>
 				<img src={Logo} alt='logomain' className={styles.logo} />
+				<img src={Bienvenido} alt='bienvenido' className={styles.Bienvenido}/>
 					<div className={styles.login_input}>
 						<input className={styles.email2} name='email' type="email" placeholder="Ingresa tu correo"></input>
 					</div>
 					<div className={styles.login__input}>
 						<input className={styles.email2} name='password' type='password' placeholder="Ingresa tu contraseña"></input>
 					</div>
-					<div className={styles.login_input }>
+					<div className={styles.login_input}>
 					<Link to="/products">
                        <button className={styles.btn_login_btn} type="submit" id="login"  >Ingresar</button>
 					</Link>
