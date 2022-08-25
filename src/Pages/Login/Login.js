8a80aa9cb18df3504eeecc55bdaaafce925118d1
unknown <mariannybatista2003@gmail.com>
2022-08-24 22:14:15 -0400

    ajustes de login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,21 +1,20 @@
 import styles from './login.module.css'
-import Logo from '../../Componentes/imagenes/logoRipley.png'
+import ripleytech from '../../Componentes/imagenes/ripleytech.png'
 import { Link } from 'react-router-dom'
 import Bienvenido from '../../Componentes/imagenes/Bienvenido.png'
 import React from 'react'
-import Header from 'Componentes/Header/Header'
 
 const Login = () => {
 	  
 	return (
 		
 		<main>
-			<Header/>
 			<div className={styles.containerImageandForm}>
 			<img src={Bienvenido} alt='bienvenido' className={styles.Bienvenido}/>
 			<div className={styles.container}>
 				<section className={styles.containerFormLogin}>
-					<img src={Logo} alt='logomain' className={styles.logo} />
+					<img src={ripleytech} alt='logoripley' className={styles.logoripleytech} />
+            		<h1 className={styles.textStyle}>RipleyTECH_</h1>
 					<input className={styles.TextBox} name='email' type="email" placeholder="Ingresa tu correo"></input>
 					<input className={styles.TextBox} name='password' type="password" placeholder="Ingresa tu contraseña"></input>
 					<Link to="/products">
