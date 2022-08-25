b17337532f38c7191a1147a8cfa4011d0fc2dbad
Camila-Arredondo <camiarredondo@MacBook-Air-de-Camila.local>
2022-08-25 08:54:52 -0400

    Tabla, placeholder, condicionales

---
+++ src/Componentes/TableProduct/TableProduct.js
@@ -0,0 +1,88 @@
+import { Stars } from "Componentes/Stars/Stars";
+import React from "react";
+import { Table } from "rsuite";
+import * as XLSX from "xlsx";
+
+export function TableProduct({ data }) {
+  const { Column, HeaderCell, Cell } = Table;
+  const downloadExcel = (data) => {
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Productos");
+    XLSX.writeFile(workbook, "Listado de Productos.xlsx");
+  };
+
+  return (
+    <>
+      <Table
+        height={400}
+        data={data}
+        onRowClick={(rowData) => {
+          console.log(rowData);
+        }}
+      >
+        <Column width={60} align="center" fixed>
+          <HeaderCell>Id</HeaderCell>
+          <Cell dataKey="id" />
+        </Column>
+
+        <Column  resizable width={150}>
+          <HeaderCell>Categoría</HeaderCell>
+          <Cell dataKey="categoria" />
+        </Column>
+
+        <Column width={150}>
+          <HeaderCell>Color</HeaderCell>
+          <Cell dataKey="color" />
+        </Column>
+
+        <Column width={100}>
+          <HeaderCell>Genero</HeaderCell>
+          <Cell dataKey="imagen1" />
+        </Column>
+
+        <Column width={100}>
+          <HeaderCell>Age</HeaderCell>
+          <Cell dataKey="age" />
+        </Column>
+
+        <Column width={150}>
+          <HeaderCell>Postcode</HeaderCell>
+          <Cell dataKey="postcode" />
+        </Column>
+
+        <Column width={300}>
+          <HeaderCell>Email</HeaderCell>
+          <Cell dataKey="email" />
+        </Column>
+        <Column width={80} fixed="right">
+          <HeaderCell>...</HeaderCell>
+          <Cell>
+            {(rowData) => (
+              <span>
+                <a onClick={() => alert(`id:${rowData.id}`)}> Edit </a>
+              </span>
+            )}
+          </Cell>
+        </Column>
+        <Column width={100} fixed="right">
+          <HeaderCell>Estrellas</HeaderCell>
+
+          <Cell>
+            {(rowData) => (
+              <Stars stars={rowData.stars} />
+            )}
+          </Cell>
+        </Column>
+      </Table>
+
+      <button
+        onClick={() => {
+          downloadExcel(data);
+        }}
+      >
+        Descargar Excel
+      </button>
+    </>
+  );
+}
--- src/Pages/NewProduct/NewProduct.js
+++ src/Pages/NewProduct/NewProduct.js
@@ -5,9 +5,26 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
 import { Stars } from "Componentes/Stars/Stars";
+import { TableProduct } from "Componentes/TableProduct/TableProduct";
 
 const NewProduct = () => {
+  const valorInicial = {
+    categoria: "",
+    SKU_seller: "",
+    título: "",
+    descripcion: "",
+    imagen1: "",
+    imagen_Miniatura: "",
+    variant_id: "",
+    marca: "",
+    color: "",
+    talla: "",
+    tabla_de_tallas: "",
+    genero: "",
+  };
   const [stars, setStars] = useState(0);
+  const [datos, setDatos] = useState(valorInicial);
+  const [grupoProductos, setGrupoProductos] = useState([]);
   const CalcularCamposCorrectos = (newData) => {
     let contador = 0;
     Object.keys(newData).map((x) => {
@@ -22,72 +39,89 @@ const NewProduct = () => {
   const campos = [
     {
       nombre: "categoria",
+      mostrar: "Categoría",
       descripcion: "A que tipo de objeto pertence el artículo",
       obligatorio: true,
+      tipo: "input",
     },
     {
       nombre: "SKU_seller",
+      mostrar: "SKU Producto",
       descripcion: "Número único que identifica su producto",
       obligatorio: true,
+      tipo: "input",
     },
-    { nombre: "título", descripcion: "Nombre del producto", obligatorio: true },
+    {
+      nombre: "título",
+      mostrar: "Nombre Producto",
+      descripcion: "Nombre del producto",
+      obligatorio: true,
+      tipo: "input",
+    },
     {
       nombre: "descripcion",
+      mostrar: "Descripción",
       descripcion: "Explique detalladamente las características del producto",
       obligatorio: true,
+      tipo: "area",
     },
     {
       nombre: "imagen1",
+      mostrar: "Imagen Principal",
       descripcion: "Agregue imagen de buena calidad",
       obligatorio: true,
+      tipo: "file",
     },
     {
       nombre: "imagen_Miniatura",
+      mostrar: "Imagen Detalle",
       descripcion: "Agregue imagen de buena calidad",
       obligatorio: true,
+      tipo: "file",
     },
     {
       nombre: "variant_id",
-      descripcion:
-        "Número que identifica la variable de su producto, de existir",
+      mostrar: "ID",
+      descripcion:"Número que identifica la variable de su producto, de existir",
       obligatorio: true,
+      tipo: "input",
     },
     {
       nombre: "marca",
+      mostrar: "Marca Producto",
       descripcion: "Nombre de la marca de su producto",
       obligatorio: true,
+      tipo: "input",
     },
-    { nombre: "color", descripcion: "Indique el color", obligatorio: true },
+    {
+      nombre: "color",
+      mostrar: "Color Producto",
+      descripcion: "Indique el color",
+      obligatorio: true,
+      tipo: "input",
+    },
     {
       nombre: "talla",
+      mostrar: "Talla Producto",
       descripcion: "Agregue talla disponible",
       obligatorio: true,
+      tipo: "input",
     },
     {
       nombre: "tabla_de_tallas",
+      mostrar: "Guía de Talla",
       descripcion: "Ingrese tabla de tallas",
       obligatorio: true,
+      tipo: "file",
     },
     {
       nombre: "genero",
+      mostrar: "Género",
       descripcion: "Agregue imagen de buena calidad",
       obligatorio: true,
+      tipo: "input",
     },
   ];
-  const [datos, setDatos] = useState({
-    categoria: "",
-    SKU_seller: "",
-    título: "",
-    descripcion: "",
-    imagen1: "",
-    imagen_Miniatura: "",
-    variant_id: "",
-    marca: "",
-    color: "",
-    talla: "",
-    tabla_de_tallas: "",
-    genero: "",
-  });
 
   const handleInputChange = (event) => {
     let newData = {
@@ -109,25 +143,55 @@ const NewProduct = () => {
           <FontAwesomeIcon icon={faArrowLeftLong} className={styles.btnBack} />
           {/* <button className={styles.btnBack}>patras</button> */}
         </Link>
-        <h1 className={styles.TilteStylePage}>¡Añade tu nuevo producto!</h1>
+        <h1 className={styles.TilteStylePage}>¡Añade tu nuevo producto!</h1><br></br>
         <p className={styles.descriptionStylePage}>Completa todos los campos</p>
       </section>
 
       <div>
-        <h1>NewProduct</h1>
+<br></br>
         <form>
           <section className={styles.container}>
             {campos.map((item, index) => (
               <div className={styles.containerAllItems} key={index}>
                 <div className={item.nombre}>
-                  <h2 className={styles.TitleStyle}>{item.nombre}</h2>
-                  <p className={styles.descriptionStyle}>{item.descripcion}</p>
-                  <input
-                    className={styles.inputcampos}
-                    name={item.nombre}
-                    type="text"
-                    onChange={handleInputChange}
-                  ></input>
+                  <h2 className={styles.TitleStyle}>{item.mostrar}</h2>
+
+                  {item.tipo === "input" ? (
+                    <input
+                      className={"rs-input"}
+                      name={item.nombre}
+                      type="text"
+                      placeholder={item.descripcion}
+                      onChange={handleInputChange}
+                      value={datos[item.nombre]}
+                    ></input>
+                  ) : item.tipo === "area" ? (
+                    <textarea
+                      className={"rs-input"}
+                      name={item.nombre}
+                      rows={2}
+                      placeholder={item.descripcion}
+                      onChange={handleInputChange}
+                      value={datos[item.nombre]}
+                    ></textarea>
+                  ) : item.tipo === "file" ? (
+                    <input
+                      className={"rs-input"}
+                      name={item.nombre}
+                      type="file"
+                      placeholder={item.descripcion}
+                      onChange={handleInputChange}
+                      value={datos[item.nombre]}
+                    ></input>
+                  ) : (
+                    <></>
+                  )}
+                 {
+                  item.obligatorio ? 
+                  datos[item.nombre] ? <></> : 
+                  <label style={{color: "red"}}>Debe llenar este campo </label> : 
+                  <></>
+                 }
                 </div>
               </div>
             ))}
@@ -135,10 +199,45 @@ const NewProduct = () => {
         </form>
         <Stars stars={stars} style={{ left: "80%" }} />
 
-        <button type="submit" className={styles.btnAddProduct}>
+        <button
+          type="button"
+          onClick={() => {
+            let isValid = true;
+            let error = "";
+            for (let i = 0; i < Object.keys(datos).length; i++) {
+              let element = Object.keys(datos)[i];
+              let isRequired = campos.find((x) => x.nombre === element);
+              if (isRequired.obligatorio) {
+                if (!datos[element]) {
+                  isValid = false;
+                  error = element;
+                  break;
+                }
+              }
+            }
+
+            if (isValid) {
+              setGrupoProductos([
+                ...grupoProductos,
+                {
+                  ...datos,
+                  id: grupoProductos.length + 1,
+                  stars: stars,
+                },
+              ]);
+              setDatos(valorInicial);
+              setStars(0);
+            }else{
+              alert('El campo ' + error + " es obligatorio");
+            }
+          }}
+          className={styles.btnAddProduct}
+        >
           Cargar
         </button>
       </div>
+
+      <TableProduct data={grupoProductos} />
     </main>
   );
 };
